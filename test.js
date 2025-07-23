51baba1752dea694bd73374e5dcf3c0ed739a749
Erwin <zhere27@gmail.com>
2025-07-23 08:43:17 +0800

    added test for create hidden sheet

--- test.js
+++ test.js
@@ -65,6 +65,38 @@ function test_excludeRecentlyCollected(forCollections, srvBank) {
   }
 }
 
+function test_createHiddenWorksheetAndAddData(forCollections, srvBank) {
+  var forCollections = [
+    ["SMART SM BATAAN", 963150.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 23"],
+    ["PLDT SAN FERNANDO LA UNION", 393850.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["PLDT TALISAY", 385390.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 23"],
+    ["SMART SF LA UNION 1", 496100.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["PLDT OLONGAPO", 378970.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 22"],
+    ["PLDT SM NORTH EDSA", 428880.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["SMART FESTIVAL MALL 2", 454150.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 22"],
+    ["SMART ROBINSONS GAPAN", 370020.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 22"],
+    ["PLDT MANDAUE", 343460.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["SMART SM CLARK 2", 500400.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 23"],
+    ["PLDT SBMA", 543400.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["PLDT PASAY", 350100.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["PLDT MANAOAG", 276200.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 23"],
+    ["SMART MARKET MARKET", 427810.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["SMART AYALA CENTER CEBU", 411120.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["PLDT JONES 1", 354140.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["SMART SM CLARK 1", 364840.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 23"],
+    ["SMART SM CEBU", 376390.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["PLDT DASMARINAS", 311930.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["SMART IBA ZAMBALES", 356110.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", ""],
+    ["SMART SM TARLAC", 251710.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 23"],
+    ["PLDT GAPAN", 100.0, "Brinks via BPI", "Brinks via BPI DPU Request - July 23, 2025 (Wednesday)", "For collection on Jul 23"]
+  ];
+
+  createHiddenWorksheetAndAddData(forCollections, 'Brinks via BPI');
+  
+}
+
+
+
 // function isTomorrowHoliday(tomorrow) {
 //   const sheetName = "StoreName Mapping";
 //   const rangeStart = "G3";
